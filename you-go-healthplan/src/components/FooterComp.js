d658b5b6a65fdb9754486dbde1053193ccd23d3e
malinsommar <malinsommar1997@gmail.com>
2020-09-29 11:34:21 +0200

    Footer responsive, contact button responsive fix

--- you-go-healthplan/src/components/FooterComp.js
+++ you-go-healthplan/src/components/FooterComp.js
@@ -8,7 +8,7 @@ import YoutubeLogo from "../images/youtubeLogo.svg";
 import YougoLogo from "../images/logo.png";
 import "../styling/footercomp.css";
 
-const FotterComponent = () => {
+const bigScreenFooter = () => {
   return (
     <div id="footerDiv">
       <div id="footer-adress">
@@ -60,6 +60,75 @@ const FotterComponent = () => {
         <img src={YougoLogo} id="footerYougoLogo" alt="Yougo healthplan Logo" />
         <p id="follow">Följ oss:</p>
       </div>
+    </div>
+  );
+};
+
+const smallScreenFooter = () => {
+  return (
+    <div className="smallScreenDiv">
+      <div id="footer-adress-small">
+        <img
+          src={YougoLogo}
+          id="footerYougoLogoSmall"
+          alt="Yougo healthplan Logo"
+        />
+        <p>Björklundabacken 10</p>
+        <p>436 57 Hovås</p>
+      </div>
+      <hr className="footer-hr" />
+      <div id="footer-contact-small">
+        <p>
+          <img className="footerIcons" src={PhoneIcon} alt="Phone icon" />
+          0707-708490
+        </p>
+
+        <p>
+          {" "}
+          <img className="footerIcons" src={MailIcon} alt="Mail icon" />
+          hej@yougohealthplan.com
+        </p>
+      </div>
+      <hr className="footer-hr" />
+      <div id="footer-media-small">
+        <a href="https://www.facebook.com/yougohealthplan/">
+          <img
+            src={FacebookLogo}
+            className="socialMediaLogoSmall"
+            alt="Facebook Logo"
+          />
+        </a>
+        <a href="https://www.instagram.com/yougohealthplan/?hl=en">
+          <img
+            src={InstagramLogo}
+            className="socialMediaLogoSmall"
+            alt="Instagram Logo"
+          />
+        </a>
+        <a href="https://se.linkedin.com/company/yougo-healthplan">
+          <img
+            src={LinkedinLogo}
+            className="socialMediaLogoSmall"
+            alt="Linkedin Logo"
+          />
+        </a>
+        <a href="https://www.youtube.com/channel/UCOOuv2oO-y79iDeU3jpx6xQ">
+          <img
+            src={YoutubeLogo}
+            className="socialMediaLogoSmall"
+            alt="Youtube Logo"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const FotterComponent = () => {
+  return (
+    <div>
+      <div id="bigFooter">{bigScreenFooter()}</div>
+      <div id="smallFooter">{smallScreenFooter()}</div>
     </div>
   );
 };
